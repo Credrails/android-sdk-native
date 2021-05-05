f7d643a84384cbbe3e084aaefab19454f9e04a74
Vincent Nwonah <vnwonah@outlook.com>
2021-05-05 16:00:06 +0100

    SubmissionsManager

---
+++ androidsdknative/src/main/java/com/credrails/androidsdknative/SubmissionsManager.java
@@ -0,0 +1,15 @@
+package com.credrails.androidsdknative;
+
+import com.credrails.androidsdknative.models.Institution;
+
+public class SubmissionsManager {
+
+    public Institution[] getInstitutions() {
+        return getInstitutions("");
+    }
+
+
+    public Institution[] getInstitutions(String countryCode) {
+        return null;
+    }
+}
---
+++ androidsdknative/src/main/java/com/credrails/androidsdknative/models/Institution.java
@@ -0,0 +1,8 @@
+package com.credrails.androidsdknative.models;
+
+public class Institution {
+    public String Code;
+    public String Name;
+    public String Country;
+    public String LogoUrl;
+}
